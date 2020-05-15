1ce632c92ce0ca7506f16ec23c16615afeb45009
Hon Tran <hontg@icd-vn.com>
2020-05-15 14:35:21 +0700

    thuc hanh vong lap

---
+++ bai5_vonglap_15_05/1.js
@@ -0,0 +1,36 @@
+﻿ var app = angular.module('myApp',['ngMaterial']);
+ app.controller('MyController',  function($scope){
+  
+  	// $scope.hienthi = false;
+  	// $scope.chuyenvesua =function(){
+  	// 	$scope.hienthi = !$scope.hienthi;
+  	// }
+
+  	$scope.hienradi = function(motnguoi){
+  		motnguoi.hienra = !motnguoi.hienra;
+  	}
+
+
+ 	$scope.nhieunguoi = [
+ 		{
+ 			ten: "hon",
+ 			namsinh:"1992",
+ 			facebook: "fb.com/hontg",
+ 			dienthoai:"0903770501",
+ 		},
+ 		{
+ 			ten: "nam",
+ 			namsinh:"1998",
+ 			facebook: "fb.com/hontg",
+ 			dienthoai:"0903xx0501",
+ 		},
+ 		{
+ 			ten: "Tuan",
+ 			namsinh:"1980",
+ 			facebook: "fb.com/Tuan",
+ 			dienthoai:"090377xxxx",
+ 		},
+
+ 	]
+
+ })
